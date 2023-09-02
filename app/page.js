22669dfe23a37b63598f1196de10448682b3ea26
Allard <AllardQuek@users.noreply.github.com>
2023-09-02 16:34:26 +0200

    Add logo to navigate home

--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <div className='container'>
+      <div className="container">
         <h1>Quiz App</h1>
-        <Link href='/create'>
-          <button>Create Quiz</button>
-        </Link>
-        <Link href='/allquizzes'>
-          <button>View all Quizzes</button>
-        </Link>
+        <div className="functions-list">
+          <Link href="/create">
+            <button className="btn-secondary btn-home">Create Quiz</button>
+          </Link>
+          <Link href="/allquizzes">
+            <button className="btn-secondary btn-home">View all Quizzes</button>
+          </Link>
+        </div>
       </div>
     </main>
   );
